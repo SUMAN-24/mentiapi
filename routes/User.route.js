9b491d88b7aaf68ea5dd0692b58914f2c79762d7
Suman Kumar Khan <99688224+SUMAN-24@users.noreply.github.com>
2023-05-28 20:42:01 +0530

    removed users route

--- routes/User.route.js
+++ routes/User.route.js
@@ -8,31 +8,22 @@ const userRouter = express.Router();
 userRouter.post('/signup', async (req, res)=>{
     const {name, email, password} = req.body;
     try{
-        bcrypt.hash(password, 5, async(err, hashed_password) => {
-            if(err){
-                console.log(err);
-            }else{
-                const user = new UserModel({name, email, password:hashed_password});
-                await user.save();
-                res.send({"msg":"Registration Successfull"});
-            }
-        });
-    }catch (err){
-        res.send({"msg":"Error while registering the user"});
-    }
-})
-
-userRouter.get('/users', async (req, res)=>{
-    const {email} = req.body;
-    try{ 
         const user = await UserModel.find({email});
         if(user.length > 0){
-            res.send({"msg": true})
+            res.send({"msg": "User already exists"});
         }else{
-            res.send({"msg": false})
-        }
+            bcrypt.hash(password, 5, async(err, hashed_password) => {
+                if(err){
+                    console.log(err);
+                }else{
+                    const user = new UserModel({name, email, password:hashed_password});
+                    await user.save();
+                    res.send({"msg":"Registration Successfull"});
+                }
+            });
+        }    
     }catch (err){
-        res.send({"msg":"Error while getting the user"});
+        res.send({"msg":"Error while registering the user"});
     }
 })
 
