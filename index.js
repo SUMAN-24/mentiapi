b2c339152cac881ae01f182367b409f2943c25a8
Suman Kumar Khan <99688224+SUMAN-24@users.noreply.github.com>
2023-05-28 16:00:47 +0530

    minor fixes

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin:"*"
+}));
 app.use(express.json());
 
 app.use(userRouter);
