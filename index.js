526f0d5d852ac74575c805e1398b6104791aef22
Suman Kumar Khan <99688224+SUMAN-24@users.noreply.github.com>
2023-05-28 15:16:48 +0530

    minor fixes

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const {connection} = require('./configs/db');
 const {userRouter} = require('./routes/User.route');
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use(userRouter);
